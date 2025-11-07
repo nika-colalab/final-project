bcfd8d692351e24f033061b9d61ee073d85d43db
nika-colalab <wallibear1999@gmail.com>
2025-11-07 17:44:26 +0400

    Add files via upload

---
+++ final profect/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random(); 
+        int number = random.Next(1, 101);
+        Console.WriteLine(number);
+    }
+}
