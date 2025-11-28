677173593380bd1621b7ef95e3d35c341e53b8fe
nika-colalab <wallibear1999@gmail.com>
2025-11-28 16:38:47 +0400

    Add files via upload

---
+++ Final Try/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("First Number ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Second Number");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Opttion (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        double result = 0;
+
+        if (op == "+")
+            result = a + b;
+        else if (op == "-")
+            result = a - b;
+        else if (op == "*")
+            result = a * b;
+        else if (op == "/")
+            result = a / b;
+
+        Console.WriteLine("Resuts: " + result);
+    }
+}
